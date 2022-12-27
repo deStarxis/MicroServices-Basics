7ce4122b2d8bdb6f1f418e126e865c83e1e1d027
deStarxis <humagainuzjwol@gmail.com>
2022-12-27 11:40:23 -0600

    added movie info service

---
+++ movie-info-service/src/main/java/com/movie/movieinfoservice/controller/MovieController.java
@@ -0,0 +1,14 @@
+package com.movie.movieinfoservice.controller;
+
+import com.movie.movieinfoservice.entity.Movie;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/movies")
+@CrossOrigin
+public class MovieController {
+    @GetMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable String movieId){
+        return new Movie(movieId,"Test Movie");
+    }
+}
