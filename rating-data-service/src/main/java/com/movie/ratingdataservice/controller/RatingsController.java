0c4750431a16282e014767d58120523db7715beb
deStarxis <humagainuzjwol@gmail.com>
2022-12-28 10:15:05 -0600

    added ratings controller

--- rating-data-service/src/main/java/com/movie/ratingdataservice/controller/RatingsController.java
+++ rating-data-service/src/main/java/com/movie/ratingdataservice/controller/RatingsController.java
@@ -3,6 +3,9 @@ package com.movie.ratingdataservice.controller;
 import com.movie.ratingdataservice.entity.Rating;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratings")
 @CrossOrigin
@@ -10,5 +13,14 @@ public class RatingsController {
     @GetMapping("/{movieId}")
     public Rating getMovieRating(@PathVariable String movieId){
         return new Rating(movieId,5);
+    }
+
+    @GetMapping("/users/{userId}")
+    public List<Rating> getUserRatings(@PathVariable String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("123",4),
+                new Rating("567",5)
+        );
+        return ratings;
     }
 }
