732a55418777b46329c895d1a6a2a2afc16689e5
deStarxis <humagainuzjwol@gmail.com>
2023-01-14 14:29:32 -0600

    eureka server and client setup

--- movie-catalog-service/src/main/java/com/movie/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/movie/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,11 +2,10 @@ package com.movie.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
     public static void main(String[] args) {
