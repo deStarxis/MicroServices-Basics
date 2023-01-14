3eb7becbf72c0e7eb5d489772af2b3c5176a3d84
deStarxis <humagainuzjwol@gmail.com>
2023-01-14 14:45:42 -0600

    added load balancer

--- movie-catalog-service/src/main/java/com/movie/moviecatalogservice/configuration/BeanConfig.java
+++ movie-catalog-service/src/main/java/com/movie/moviecatalogservice/configuration/BeanConfig.java
@@ -1,5 +1,6 @@
-package com.movie.moviecatalogservice;
+package com.movie.moviecatalogservice.configuration;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class BeanConfig {
     //    creating beans
     @Bean
+    @LoadBalanced // service discovery in a load balanced way looks for the service name hints
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
--- movie-catalog-service/src/main/java/com/movie/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/movie/moviecatalogservice/controller/MovieCatalogController.java
@@ -4,6 +4,7 @@ import com.movie.moviecatalogservice.entity.CatalogItem;
 import com.movie.moviecatalogservice.entity.Movie;
 import com.movie.moviecatalogservice.entity.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,15 +17,19 @@ import java.util.stream.Collectors;
 public class MovieCatalogController {
     @Autowired
     private RestTemplate restTemplate;
+
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable String userId){
 //        using this for calling the API
 //        using rest template
 //        both of these are synchronous
         //get all rated movie ids
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratings/users/"+userId, UserRating.class); //1
+        UserRating ratings = restTemplate.getForObject("http://rating-data-service/ratings/users/"+userId, UserRating.class); //1
         return ratings.getRatings().stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class); // 2
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class); // 2
             return new CatalogItem(movie.getName(),"This is the description",rating.getRating());
         }).collect(Collectors.toList());
     }
