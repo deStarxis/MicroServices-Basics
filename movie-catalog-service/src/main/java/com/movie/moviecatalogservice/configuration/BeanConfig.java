1cb48f179fc0f48bc43b46f8a16525b2621bf3bb
deStarxis <humagainuzjwol@gmail.com>
2023-01-27 14:40:46 -0600

    added bean configuration with timeouts

--- movie-catalog-service/src/main/java/com/movie/moviecatalogservice/configuration/BeanConfig.java
+++ movie-catalog-service/src/main/java/com/movie/moviecatalogservice/configuration/BeanConfig.java
@@ -3,6 +3,7 @@ package com.movie.moviecatalogservice.configuration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -12,7 +13,11 @@ public class BeanConfig {
     @Bean
     @LoadBalanced // service discovery in a load balanced way looks for the service name hints
     public RestTemplate getRestTemplate(){
-        return new RestTemplate();
+//        return new RestTemplate();
+//        using request timeout
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(3000);
+        return new RestTemplate(clientHttpRequestFactory);
     }
     @Bean
     public WebClient.Builder getWebClientBuilder(){
